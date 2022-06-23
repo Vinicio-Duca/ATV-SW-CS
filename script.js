036f9d61e450d76b0384c159b5a86cf4334b5b02
Vinicio Duca Bandeira <99352736+Vinicio-Duca@users.noreply.github.com>
2022-06-22 21:46:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+function resposta1(){
+    let valor = 1;
+    switch(valor){
+        case 1: {
+            let div1 = document.getElementById("txt1");
+            div1.classList.remove("re");
+            div1.classList.add("pe0");
+            let div2 = document.getElementById("txt2");
+            div2.classList.remove("pe0");
+            div2.classList.add("re");
+            let div3 = document.getElementById("txt3");
+            div3.classList.remove("pe0");
+            div3.classList.add("re");
+            let div4 = document.getElementById("txt4");
+            div4.classList.remove("pe0");
+            div4.classList.add("re");
+            break;
+        }
+    }
+}
+function resposta2(){
+    let valor = 2;
+    switch(valor){
+        case 2: {
+            let div1 = document.getElementById("txt1");
+            div1.classList.remove("pe0");
+            div1.classList.add("re");
+            let div2 = document.getElementById("txt2");
+            div2.classList.remove("re");
+            div2.classList.add("pe0");
+            let div3 = document.getElementById("txt3");
+            div3.classList.remove("pe0");
+            div3.classList.add("re");
+            let div4 = document.getElementById("txt4");
+            div4.classList.remove("pe0");
+            div4.classList.add("re");
+            break;
+        }
+    }
+}
+function resposta3(){
+    let valor = 3;
+    switch(valor){
+        case 3: {
+            let div1 = document.getElementById("txt1");
+            div1.classList.remove("pe0");
+            div1.classList.add("re");
+            let div2 = document.getElementById("txt2");
+            div2.classList.remove("pe0");
+            div2.classList.add("re");
+            let div3 = document.getElementById("txt3");
+            div3.classList.remove("re");
+            div3.classList.add("pe0");
+            let div4 = document.getElementById("txt4");
+            div4.classList.remove("pe0");
+            div4.classList.add("re");
+            break;
+        }
+    }
+}
+function resposta4(){
+    let valor = 4;
+    switch(valor){
+        case 4: {
+            let div1 = document.getElementById("txt1");
+            div1.classList.remove("pe0");
+            div1.classList.add("re");
+            let div2 = document.getElementById("txt2");
+            div2.classList.remove("pe0");
+            div2.classList.add("re");
+            let div3 = document.getElementById("txt3");
+            div3.classList.remove("pe0");
+            div3.classList.add("re");
+            let div4 = document.getElementById("txt4");
+            div4.classList.remove("re");
+            div4.classList.add("pe0");
+            break;
+        }
+    }
+}
